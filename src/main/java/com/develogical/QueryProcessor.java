51eb6018eb84cb99be6f88cf1fec330df0cdf353
Jen <jen@dyn3194-76.wlan.ic.ac.uk>
2020-01-21 11:47:45 +0000

    James Bond Banana

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -42,6 +42,17 @@ public class QueryProcessor {
             }
             return Integer.toString(Collections.max(numberList));
         }
+
+        if (query.toLowerCase().contains("who played James Bond in the film Dr No")) {
+            return "Sean Connery";
+        }
+
+        if (query.toLowerCase().contains("what colour is a banana")) {
+            return "Yellow";
+        }
+
+
+
         return "";
     }
 }
