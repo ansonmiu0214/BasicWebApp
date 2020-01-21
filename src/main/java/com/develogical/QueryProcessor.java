3147c05bf4db00c90b5df6636481624dd94db022
Jen <jen@dyn3194-76.wlan.ic.ac.uk>
2020-01-21 11:35:33 +0000

    largest number

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,8 @@
 package com.develogical;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -11,6 +14,15 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        }
+
+        if (query.toLowerCase().contains("which of the% following numbers is the largest:")) {
+            String numbers = query.substring(query.lastIndexOf(":") + 1);
+            List<Integer> numberList = new ArrayList<>();
+            for (String s : numbers.split(", ")) {
+                numberList.add(Integer.parseInt(s));
+            }
+
         }
         return "";
     }
